3893cf4f1e90d338176de4f89ffa8387a3d8f8c4
azapdev <a7mad3zap@gmail.com>
2026-01-31 21:43:02 +0200

    main edite

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Index from "./layout/Index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <Index />
     </BrowserRouter>
   </StrictMode>,
-)
+);
