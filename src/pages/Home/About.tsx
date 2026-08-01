d0c0927742379ac98e457b41302f5e978b7d5579
azapdev <a7mad3zap@gmail.com>
2026-08-02 00:56:17 +0200

    add CV

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -98,83 +98,89 @@ const About: FC = () => {
     },
   ];
   return (
-    <>
-      <div
-        id="about"
-        className="mx-auto my-14 grid h-full max-w-6xl grid-cols-1 items-center justify-center gap-5 overflow-hidden bg-black/80 p-4 lg:grid-cols-[70%_30%]"
-      >
-        {/* info */}
-        <div className="flex flex-col items-start gap-6 md:ms-8 md:p-5">
-          {/* ABOUT ME */}
-          <motion.div
-            initial={{ y: -100, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ type: 'spring', stiffness: 100, duration: 1 }}
-            viewport={{ once: true }}
-          >
-            <h1 className="text-main flex items-baseline gap-3 text-4xl font-bold md:text-6xl">
-              <span className="h-7 w-2 rounded-3xl bg-white text-white md:h-10 md:text-5xl"></span>
-              {t('common:ABOUTpage.ABOUT_ME')}
-            </h1>
-            <p className="text-sec-title px-5 py-4 sm:px-9">
-              {t('common:ABOUTpage.DESCRIPTION')}
-            </p>
-          </motion.div>
-
-          {/* SKILLS */}
-          <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            whileInView={{ y: 0, opacity: 1 }}
-            transition={{ type: 'spring', stiffness: 100, duration: 1 }}
-            viewport={{ once: true }}
-          >
-            <h2 className="flex items-baseline gap-2 text-2xl font-bold text-white">
-              <span className="h-5 w-1 rounded-3xl bg-white text-white md:h-7 md:w-1"></span>
-              <span className="text-main text-2xl font-bold md:text-3xl">
-                {t('common:ABOUTpage.SKILL')}
-              </span>
-              {t('common:ABOUTpage.TECHNOLOGIES')}
-            </h2>
-            {/* tec */}
-            <div className="sm:grid-cols-3 mt-8 grid grid-cols-2 gap-3 md:grid-cols-4">
-              {skills.map((skill, i) => (
-                <a
-                  key={i}
-                  href={skill.link}
-                  className="group flex w-fit items-center gap-2"
-                >
-                  <Icon
-                    icon={skill.icon}
-                    className="text-gray-300 opacity-50 grayscale-100 group-hover:opacity-100"
-                  />
-
-                  <span className="group-hover:text-main text-sm tracking-[1.5px] text-white brightness-50 transition duration-200 group-hover:brightness-125">
-                    {skill.label}
-                  </span>
-                </a>
-              ))}
-            </div>
-          </motion.div>
-        </div>
-
-        {/* img */}
+    <section
+      id="about"
+      className="mx-auto my-14 grid h-full max-w-6xl grid-cols-1 items-center justify-center gap-5 overflow-hidden bg-black/80 p-4 lg:grid-cols-[70%_30%]"
+    >
+      {/* info */}
+      <div className="flex flex-col items-start gap-6 md:ms-8 md:p-5">
+        {/* ABOUT ME */}
         <motion.div
-          className="order-first flex items-center justify-center object-contain lg:order-1"
-          initial={{ x: 100, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ y: -100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
           transition={{ type: 'spring', stiffness: 100, duration: 1 }}
           viewport={{ once: true }}
         >
-          <img
-            src="/img/me.webp"
-            alt=""
-            className="flex w-50 justify-center rounded-2xl"
-            loading="lazy"
-            fetchPriority="high"
-          />
+          <h1 className="text-main flex items-baseline gap-3 text-4xl font-bold md:text-6xl">
+            <span className="h-7 w-2 rounded-3xl bg-white text-white md:h-10 md:text-5xl"></span>
+            {t('common:ABOUTpage.ABOUT_ME')}
+          </h1>
+          <p className="text-sec-title px-5 py-4 sm:px-9">
+            {t('common:ABOUTpage.DESCRIPTION')}
+          </p>
+        </motion.div>
+
+        {/* SKILLS */}
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ type: 'spring', stiffness: 100, duration: 1 }}
+          viewport={{ once: true }}
+          className="flex flex-col items-start gap-4"
+        >
+          <h2 className="flex items-baseline gap-2 text-2xl font-bold text-white">
+            <span className="h-5 w-1 rounded-3xl bg-white text-white md:h-7 md:w-1"></span>
+            <span className="text-main text-2xl font-bold md:text-3xl">
+              {t('common:ABOUTpage.SKILL')}
+            </span>
+            {t('common:ABOUTpage.TECHNOLOGIES')}
+          </h2>
+          {/* tec */}
+          <div className="mt-8 grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4">
+            {skills.map((skill, i) => (
+              <a
+                key={i}
+                href={skill.link}
+                className="group flex w-fit items-center gap-2"
+              >
+                <Icon
+                  icon={skill.icon}
+                  className="text-gray-300 opacity-50 grayscale-100 group-hover:opacity-100"
+                />
+
+                <span className="group-hover:text-main text-sm tracking-[1.5px] text-white brightness-50 transition duration-200 group-hover:brightness-125">
+                  {skill.label}
+                </span>
+              </a>
+            ))}
+          </div>
+          <a
+            href="/CV/Ahmed Elazap - Frontend Developer.pdf"
+            target="_blank"
+            className="border-main bg-main hover:text-main w-fit md:mt-8 rounded-full border-2 px-2 py-1 text-lg font-light text-black transition duration-150 hover:bg-transparent md:px-6 md:py-4"
+          >
+            Download CV
+          </a>
         </motion.div>
       </div>
-    </>
+
+      {/* img */}
+      <motion.div
+        className="order-first flex items-center justify-center object-contain lg:order-1"
+        initial={{ x: 100, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ type: 'spring', stiffness: 100, duration: 1 }}
+        viewport={{ once: true }}
+      >
+        <img
+          src="/img/me.webp"
+          alt=""
+          className="flex w-50 justify-center rounded-2xl"
+          loading="lazy"
+          fetchPriority="high"
+        />
+      </motion.div>
+    </section>
   );
 };
 
